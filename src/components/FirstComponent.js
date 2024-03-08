e5e26b609410e65eb986f5a03731254290857e43
=Lemi-Melk <=lemimelkamu@gmail.com>
2024-03-08 17:29:50 +0300

    first commit

---
+++ src/components/FirstComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../Style/firstcomp.css'
+function FirstComponent(){
+
+return (
+    <div id='my-div'>
+        <p id='my-p'>Hello this is my first </p>
+    </div>
+);
+
+};
+
+export default FirstComponent;
