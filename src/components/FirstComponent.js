025757705ffc4ab0d1604052eb475731ccf62308
=Lemi-Melk <=lemimelkamu@gmail.com>
2024-03-09 10:14:17 +0300

    fist commit

--- src/components/FirstComponent.js
+++ src/components/FirstComponent.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import '../Style/firstcomp.css'
 function FirstComponent(){
+let js_obj = ["first","second","third","fourth"];
+function display_data(value,index,array){
+   return(
+    <li id={index}>{value}</li>
 
+   );    
+}
 return (
-    <div id='my-div'>
+    <div class='my-div'>
         <p id='my-p'>Hello this is my first </p>
-    </div>
+        <div class='inner-div'>
+        <ul>
+         <li><h1>The First List</h1></li>
+        {js_obj.forEach(display_data)}   
+        </ul>
+        </div>
+
+   </div>     
 );
 
 };
